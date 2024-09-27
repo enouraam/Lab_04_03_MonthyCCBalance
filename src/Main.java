192c03d959a05014e7b637fa5dea99d0e0110c98
enouraam <enouraam@mail.uc.edu>
2024-09-27 14:17:11 -0400

    commit/complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double balance = 5000;
+        double INTEREST_RATE = .17;
+        double oneMonth = balance * INTEREST_RATE;
+        double twoMonth = (balance + oneMonth) * INTEREST_RATE;
+
+        System.out.println("your interest due after one month "+oneMonth);
+        System.out.println("your interest due after two months "+twoMonth);
     }
 }
